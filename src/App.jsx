7515719566f9d0bfaa4f8420a69828212d51bc5b
Sky Walker <meisskywalker@proton.me>
2023-02-07 23:05:07 +0700

    fix: fix routing problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
 import Nav from "./components/Nav/Nav.component"
 import "./App.css"
 import HomeView from "./views/Home/Home.view"
